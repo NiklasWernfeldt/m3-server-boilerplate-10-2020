a9cd0adf0e7afa2018859853c8de4fe484f135cb
Niklas Wernfeldt <niklas.wernfeldt.94@gmail.com>
2020-12-14 23:33:35 +0100

    add a get user route

--- routes/user.router.js
+++ routes/user.router.js
@@ -11,10 +11,20 @@ const {
 
   router.put("/:id", isLoggedIn ,(req, res, next) => {
       const {id} = req.params
-      const {firstname, lastname, email, password, profileImage} = req.body
-      User.findByIdAndUpdate(id, {firstname, lastname, email, password, profileImage}, {new: true})
+      const {firstname, lastname, email, password, profileImage} = req.body      // should this be a $set, is thats why GET user is not working?
       .then((updatedUser) => {
         res.status(200).json({updatedUser})
+      })
+      .catch((err) => next(createError(err)))
+  })
+
+  router.get("/:id", isLoggedIn, (req, res, next) => {
+    console.log("user.router")
+      const {id} = req.params
+      User.findById(id)
+      .then((foundUser) => {
+        console.log("foundUser",foundUser)
+          res.status(200).json(foundUser)
       })
       .catch((err) => next(createError(err)))
   })
